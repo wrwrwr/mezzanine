724766240ebe094802054c09fd09210d62987356
wrwrwr <git@wr.waw.pl>
2013-04-17 08:25:44 +0200

    Update_generated_fields command now may also reset all slugs (thus regenerating them from titles).

--- mezzanine/core/management/commands/update_generated_fields.py
+++ mezzanine/core/management/commands/update_generated_fields.py
@@ -1,21 +1,39 @@
 
+from optparse import make_option
+
 from django.core.management.base import NoArgsCommand
 from django.db.models import get_models
 
 from mezzanine.core.models import Slugged, MetaData
 from mezzanine.pages.models import Page
+from mezzanine.utils.translation import for_all_languages
 
 
 class Command(NoArgsCommand):
-
-    help = ("Loads and saves all models that have some fields generated"
-            " automatically (slugs, descriptions, titles) to let them"
-            " generate values for new languages.")
+    option_list = NoArgsCommand.option_list + (
+        make_option("--reset_slugs", action="store_true", default=False,
+            help=("With \"reset_slugs\" argument it will reset all slugs, "
+                  "allowing them to be regenerated automatically (albeit, "
+                  "forefeiting any customizations!). May need to be run a "
+                  "few times to get all path chains sound.")),)
+    help = ("Loads and saves all models that have some fields generated "
+            "automatically (slugs, descriptions, titles) to let them "
+            "generate values for new languages.")
 
     def handle_noargs(self, **options):
         verbosity = int(options.get("verbosity", 0))
+        reset_slugs = options.get("reset_slugs")
 
         for model in get_models():
+            if reset_slugs and issubclass(model, Slugged):
+                if verbosity > 0:
+                    print "Reseting slugs of %s" % model.__name__
+                for instance in model.objects.all():
+                    def reset_slug():
+                        instance.slug = None
+                    for_all_languages(reset_slug)
+                    instance.save()
+
             if issubclass(model, (Slugged, MetaData, Page)):
                 if verbosity > 0:
                     print "Updating %s" % model.__name__
